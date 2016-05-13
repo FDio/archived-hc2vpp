3eecfd9e241c50c9e498391e706a4be1240f2cd1
Maros Marsalek <mmarsale@cisco.com>
2016-05-13 11:24:15 +0200

    HONEYCOMB-61: Make InmemoryDataTree module type configurable
    Change-Id: Ie73eac8960a76ebcc578d3b44c50492f29908e24
    Signed-off-by: Maros Marsalek <mmarsale@cisco.com>

--- v3po/data-impl/src/main/java/org/opendaylight/yang/gen/v1/urn/honeycomb/params/xml/ns/yang/data/impl/rev160411/InMemoryDataTreeModule.java
+++ v3po/data-impl/src/main/java/org/opendaylight/yang/gen/v1/urn/honeycomb/params/xml/ns/yang/data/impl/rev160411/InMemoryDataTreeModule.java
@@ -1,26 +1,21 @@
 package org.opendaylight.yang.gen.v1.urn.honeycomb.params.xml.ns.yang.data.impl.rev160411;
 
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
-import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTree;
-import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidate;
-import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeModification;
-import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeSnapshot;
-import org.opendaylight.yangtools.yang.data.api.schema.tree.DataValidationFailedException;
-import org.opendaylight.yangtools.yang.data.api.schema.tree.TreeType;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.*;
 import org.opendaylight.yangtools.yang.data.impl.schema.tree.InMemoryDataTreeFactory;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class InMemoryConfigDataTreeModule extends org.opendaylight.yang.gen.v1.urn.honeycomb.params.xml.ns.yang.data.impl.rev160411.AbstractInMemoryConfigDataTreeModule {
+public class InMemoryDataTreeModule extends org.opendaylight.yang.gen.v1.urn.honeycomb.params.xml.ns.yang.data.impl.rev160411.AbstractInMemoryDataTreeModule {
 
-    private static final Logger LOG = LoggerFactory.getLogger(InMemoryConfigDataTreeModule.class);
+    private static final Logger LOG = LoggerFactory.getLogger(InMemoryDataTreeModule.class);
 
-    public InMemoryConfigDataTreeModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+    public InMemoryDataTreeModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
         super(identifier, dependencyResolver);
     }
 
-    public InMemoryConfigDataTreeModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.yang.gen.v1.urn.honeycomb.params.xml.ns.yang.data.impl.rev160411.InMemoryConfigDataTreeModule oldModule, java.lang.AutoCloseable oldInstance) {
+    public InMemoryDataTreeModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.yang.gen.v1.urn.honeycomb.params.xml.ns.yang.data.impl.rev160411.InMemoryDataTreeModule oldModule, java.lang.AutoCloseable oldInstance) {
         super(identifier, dependencyResolver, oldModule, oldInstance);
     }
 
@@ -32,14 +27,17 @@ public class InMemoryConfigDataTreeModule extends org.opendaylight.yang.gen.v1.u
     @Override
     public java.lang.AutoCloseable createInstance() {
         LOG.debug("InMemoryConfigDataTreeModule.createInstance()");
-        return new CloseableConfigDataTree(getSchemaServiceDependency().getGlobalContext());
+        return new CloseableConfigDataTree(getSchemaServiceDependency().getGlobalContext(), getType());
     }
 
-    private static class CloseableConfigDataTree implements AutoCloseable, DataTree {
-        private final DataTree dataTree;
+    private static class CloseableConfigDataTree implements AutoCloseable,
+        org.opendaylight.yangtools.yang.data.api.schema.tree.DataTree {
+        private final org.opendaylight.yangtools.yang.data.api.schema.tree.DataTree dataTree;
 
-        public CloseableConfigDataTree(final SchemaContext schemaContext) {
-            this.dataTree = InMemoryDataTreeFactory.getInstance().create(TreeType.CONFIGURATION);
+        public CloseableConfigDataTree(final SchemaContext schemaContext, final DatatreeType type) {
+            this.dataTree = InMemoryDataTreeFactory.getInstance().create(
+                type == DatatreeType.Config ? TreeType.CONFIGURATION : TreeType.OPERATIONAL
+            );
             dataTree.setSchemaContext(schemaContext);
         }
 
