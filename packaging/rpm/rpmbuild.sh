abad08ac35f8f62c6773da9409ccc458689f5b5e
Ed Warnicke <eaw@cisco.com>
2016-06-17 14:54:04 -0500

    Fix for rpmbuild
    Change-Id: I8d7f1c17d750db72a7344b3c670ed850d5b90dff
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

--- packaging/rpm/rpmbuild.sh
+++ packaging/rpm/rpmbuild.sh
@@ -7,4 +7,6 @@ ZIPFILE=${ZIPDIR}/v3po-karaf-1.0.0-SNAPSHOT.zip
 mkdir -p ${DIR}/SOURCES/
 cp $ZIPFILE ${DIR}/SOURCES/
 cp ${DIR}/honeycomb.spec ${DIR}/SOURCES/
+cd ${DIR}
 rpmbuild -bb --define "_topdir ${DIR}"  ${DIR}/honeycomb.spec
+cd -
