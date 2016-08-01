84eb5829ae847a08db1e6fd08de1be51762392a4
Maros Marsalek <mmarsale@cisco.com>
2016-08-01 07:02:13 -0400

    HONEYCOMB-130: Update rpm build to use vpp-integration karaf
    Change-Id: Id808bc72887cfd40f322266d95d21a2ef77f59ce
    Signed-off-by: Maros Marsalek <mmarsale@cisco.com>

--- packaging/rpm/rpmbuild.sh
+++ packaging/rpm/rpmbuild.sh
@@ -2,8 +2,8 @@
 set -xe
 DIR=$(dirname $0)
 DIR=$(readlink -f $DIR)
-ZIPDIR=${DIR}/../../v3po/karaf/target/
-ZIPFILE=${ZIPDIR}/v3po-karaf-1.0.0-SNAPSHOT.zip
+ZIPDIR=${DIR}/../../vpp-integration/karaf/target/
+ZIPFILE=${ZIPDIR}/vpp-integration-karaf-1.0.0-SNAPSHOT.zip
 mkdir -p ${DIR}/SOURCES/
 cp $ZIPFILE ${DIR}/SOURCES/
 cp ${DIR}/honeycomb.spec ${DIR}/SOURCES/
