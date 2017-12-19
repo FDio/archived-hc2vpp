a31bc791c7dca9ab9f74691fb8774f36be17764e
Marek Gradzki <mgradzki@cisco.com>
2017-12-19 10:19:43 +0100

    Scripts for installing jvpp jars to local mvn repo
    By default hc2vpp build downloads latest jvpp
    packages from nexus.fd.io.
    They might not be compatible with installed
    vpp (e.g. in case of non-backward
    compatible vpp api changes).
    
    In order to prevent API mismatch,
    it is required to use jvpp version corresponding
    to the vpp installed.
    
    The patch includes scripts that can be used
    before building hc2vpp to install jvpp jars
    from vpp build dir or vpp-api-java package.
    
    Change-Id: I9aa0dc37921e022e5644c54238d3d579f6eb5ba8
    Signed-off-by: Marek Gradzki <mgradzki@cisco.com>

---
+++ jvpp/common.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Installs jvpp jar to local maven repository.
+#
+# $1 - jvpp jar file path
+# $1 - target artifact version
+#
+function install_jvpp_jar {
+  jarfile=$1
+  version=$2
+
+  # Filename (includes version suffix), e.g. jvpp-core-18.01
+  basefile=$(basename -s .jar "$jarfile")
+
+  # Remove version suffix
+  artifactId=$(echo "$basefile" | rev | cut -d '-' -f 2- | rev)
+
+  mvn install:install-file \
+    -Dfile=$jarfile \
+    -DgroupId=io.fd.vpp \
+    -DartifactId=$artifactId \
+    -Dversion=$version \
+    -Dpackaging=jar
+}
