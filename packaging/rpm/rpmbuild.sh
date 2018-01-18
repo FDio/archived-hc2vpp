fc3841ee6a7b1559a9819b608e03e2c54bffeb10
Marek Gradzki <mgradzki@cisco.com>
2018-01-18 12:03:34 +0100

    Make rpmbuild.sh use hc2vpp_version
    Another step towards hc2vpp release process.
    
    Change-Id: I873782b79368bbf7ee569920378ad53651802e37
    Signed-off-by: Marek Gradzki <mgradzki@cisco.com>

--- packaging/rpm/rpmbuild.sh
+++ packaging/rpm/rpmbuild.sh
@@ -2,8 +2,9 @@
 set -xe
 DIR=$(dirname $0)
 DIR=$(readlink -f $DIR)
+HC2VPP_VERSION=$(${DIR}/hc2vpp_version)
 ZIPDIR=${DIR}/../../vpp-integration/minimal-distribution/target/
-ZIPFILE=${ZIPDIR}/vpp-integration-distribution-1.18.01-SNAPSHOT-hc.zip
+ZIPFILE=${ZIPDIR}/vpp-integration-distribution-${HC2VPP_VERSION}-hc.zip
 mkdir -p ${DIR}/SOURCES/
 cp $ZIPFILE ${DIR}/SOURCES/
 cp ${DIR}/honeycomb.spec ${DIR}/SOURCES/
