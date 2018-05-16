032160337ff39c42bcb536cc50cd83ebac0b6106
Marek Gradzki <mgradzki@cisco.com>
2018-05-16 14:04:35 +0200

    docker: fix path in copy_packages.sh
    Change-Id: I737000a63c81d6812a8fb6aa6d4d8d6b87ffdc7d
    Signed-off-by: Marek Gradzki <mgradzki@cisco.com>
    (cherry picked from commit 03f6001049d60c59c81408c472996cba4e703a75)

--- examples/docker/copy_packages.sh
+++ examples/docker/copy_packages.sh
@@ -3,4 +3,4 @@
 rm packages/*.deb
 
 # Copies locally-built vpp and hc2vpp packages
-cp ~/vpp/build-root/*.deb ../packaging/deb/xenial/*.deb packages/
+cp ~/vpp/build-root/*.deb ../../packaging/deb/xenial/*.deb packages/
