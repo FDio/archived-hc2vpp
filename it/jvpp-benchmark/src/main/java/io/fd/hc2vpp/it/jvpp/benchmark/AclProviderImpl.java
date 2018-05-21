d3cc46c357c83c0c0b41b4f64b166bbb40b74743
Marek Gradzki <mgradzki@cisco.com>
2018-05-21 14:43:45 +0200

    JVpp JMH benchmark
    Creates ACL of size aclSize using acl_add_replace,
    then assigns it to local0 using acl_interface_set_acl_list.
    
    Then ACL is updated synchronously using acl_add_replace.
    By default 20x2s warmup and 100x2s measurment iterations
    are performed.
    
    VPP is restarted after each iteration.
    
    Each invocation of acl_add_replace uses ACL from precomputed
    set of ACLs of size aclSetSize.
    ACLs from the set are used in round-robin fashion.
    
    Compile:
    
    cd $HC2VPP_ROOT/it/jvpp-benchmark
    mvn clean install
    
    Run with:
    
    sudo java -jar ./target/jvpp-benchmark-exec.jar
    
    To specify aclSize (default=100), use:
    
    sudo java -jar ./target/jvpp-benchmark-exec.jar -p aclSize=1000
    
    To specify aclSetSize (default=100), use:
    
    sudo java -jar ./target/jvpp-benchmark-exec.jar -p aclSetSize=1000
    
    To see more options, use
    
    java -jar ./target/jvpp-benchmark-exec.jar -h
    
    Change-Id: I43691ba891940a1c341f8e9893c8598c811bc077
    Signed-off-by: Marek Gradzki <mgradzki@cisco.com>

---
+++ it/jvpp-benchmark/src/main/java/io/fd/hc2vpp/it/jvpp/benchmark/AclProviderImpl.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2018 Cisco and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.fd.hc2vpp.it.jvpp.benchmark;
+
+import io.fd.vpp.jvpp.acl.dto.AclAddReplace;
+import io.fd.vpp.jvpp.acl.types.AclRule;
+import javax.annotation.concurrent.NotThreadSafe;
+
+@NotThreadSafe
+class AclProviderImpl implements AclProvider {
+    private static final int CREATE_NEW_ACL = -1;
+    private static final short MAX_PORT_NUMBER = (short) 65535;
+
+    private final int aclSetSize;
+    private final AclAddReplace[] acls;
+
+    /**
+     * Pointer to ACL to be returned by invocation of {@link #next()} method.
+     */
+    private int currentAcl = 0;
+
+    AclProviderImpl(final int aclSetSize, final int aclSize) {
+        this.aclSetSize = aclSetSize;
+        acls = new AclAddReplace[aclSetSize];
+        initAcls(aclSetSize, aclSize);
+    }
+
+    @Override
+    public AclAddReplace next() {
+        final AclAddReplace result = acls[currentAcl];
+        currentAcl = (currentAcl + 1) % aclSetSize;
+        return result;
+    }
+
+    @Override
+    public void setAclIndex(final int index) {
+        for (int i = 0; i < aclSetSize; ++i) {
+            acls[i].aclIndex = index;
+        }
+    }
+
+    private void initAcls(final int aclSetSize, final int aclSize) {
+        long ip = 0x01000000; // 1.0.0.0
+        for (int i = 0; i < aclSetSize; ++i) {
+            acls[i] = createAddReplaceRequest(aclSize, CREATE_NEW_ACL, (int) (ip++));
+        }
+    }
+
+    private static AclAddReplace createAddReplaceRequest(final int size, final int index, final int ip) {
+        AclAddReplace request = new AclAddReplace();
+        request.aclIndex = index;
+        request.count = size;
+        request.r = createRules(size, ip);
+        return request;
+    }
+
+    private static AclRule[] createRules(final int size, final int ip) {
+        final AclRule[] rules = new AclRule[size];
+        for (int i = 0; i < size; ++i) {
+            rules[i] = new AclRule();
+            rules[i].isIpv6 = 0;
+            rules[i].isPermit = 1;
+            rules[i].srcIpAddr = getIp(ip);
+            rules[i].srcIpPrefixLen = 32;
+            rules[i].dstIpAddr = getIp(ip);
+            rules[i].dstIpPrefixLen = 32;
+            rules[i].dstportOrIcmpcodeFirst = 0;
+            rules[i].dstportOrIcmpcodeLast = MAX_PORT_NUMBER;
+            rules[i].srcportOrIcmptypeFirst = 0;
+            rules[i].srcportOrIcmptypeLast = MAX_PORT_NUMBER;
+            rules[i].proto = 17; // UDP
+        }
+        return rules;
+    }
+
+    private static byte[] getIp(final int i) {
+        int b1 = (i >> 24) & 0xff;
+        int b2 = (i >> 16) & 0xff;
+        int b3 = (i >> 8) & 0xff;
+        int b4 = i & 0xff;
+        return new byte[] {(byte) b1, (byte) b2, (byte) b3, (byte) b4};
+    }
+}
