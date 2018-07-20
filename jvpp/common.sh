e07c85cf527425eeb452388af426d5390aa5a6ff
Marek Gradzki <mgradzki@cisco.com>
2018-07-20 13:47:32 +0200

    Bump jvpp version to 18.10-SNAPSHOT
    Also sets version of VPP package dependencies to 18.07.
    
    Change-Id: I4be0a5a9217c3e059125e7eb5a36170d02f12b88
    Signed-off-by: Marek Gradzki <mgradzki@cisco.com>

--- jvpp/common.sh
+++ jvpp/common.sh
@@ -9,7 +9,7 @@ function install_jvpp_jar {
   jarfile=$1
   version=$2
 
-  # Filename (includes version suffix), e.g. jvpp-core-18.07
+  # Filename (includes version suffix), e.g. jvpp-core-18.10
   basefile=$(basename -s .jar "$jarfile")
 
   # Remove version suffix
