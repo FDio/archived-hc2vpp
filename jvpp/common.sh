d98af41d7030e012f6e6c2206786662ab8d268d6
Michal Cmarada <mcmarada@cisco.com>
2019-01-24 20:17:04 +0100

    Bump jvpp version to 19.04-SNAPSHOT
    Changes
     - sets version of VPP package dependencies to 19.04.
     - updates vpp-plugins to vpp-plugin-core dependencies for debian
    
    Change-Id: Ida51151910f217248aa761b59f8cfdb3861b3f26
    Signed-off-by: Michal Cmarada <mcmarada@cisco.com>

--- jvpp/common.sh
+++ jvpp/common.sh
@@ -9,7 +9,7 @@ function install_jvpp_jar {
   jarfile=$1
   version=$2
 
-  # Filename (includes version suffix), e.g. jvpp-core-19.01
+  # Filename (includes version suffix), e.g. jvpp-core-19.04
   basefile=$(basename -s .jar "$jarfile")
 
   # Remove version suffix
